86b21583d9655ba7f22ad5fe5fc6a7da39006986
prasanth <santhtce@gmail.com>
2017-09-07 01:34:29 -0300

    Added wrapper for gopro; Stream the live feed over UDP; Capture the feed in the browser

---
+++ Readme.md
@@ -0,0 +1,17 @@
+# Go Pro Hero 4 Session
+
+## Camera:
+  - Set the Camera in the APP mode
+
+## Server:
+  - Install python 3.6
+  - Set pip in the Environment variable
+  - cd server
+  - pip install -r requirements.txt
+  - python gopro.py
+
+## Client:
+  - cd client
+  - Run Mongoose server (just execute it)
+ 
+### See the live feed at 10.5.5.101:8080
---
+++ server/gopro.py
@@ -0,0 +1,41 @@
+from flask import Flask, abort, request 
+import time
+import _thread
+from goprocam import GoProCamera
+from goprocam import constants
+import base64
+import re
+
+
+app = Flask(__name__)
+print("Establishing Connection with the Camera")
+#gopro = GoProCamera.GoPro(constants.gpcontrol)
+	
+		
+def stream():
+	print("Starting the live stream")
+	gopro.livestream("start")
+	gopro.stream("udp://127.0.0.1:1000", "high")
+	print("Stopping the live stream")
+
+
+@app.route('/')
+def hello():
+	return "Hello World"
+
+@app.route('/screenshot', methods=['POST'])
+def get_image():
+    image_b64 = request.values['imageBase64']
+    order_id = request.values['orderid']
+    print(image_b64)
+    print(order_id)
+    k = base64.b64encode(image_b64.encode())
+    with open("imageToSave.jpg", "wb") as fh:
+    	fh.write(base64.decodebytes(k))
+    return ''
+
+if __name__ == '__main__':
+	print("Starting the Web Server")
+	#_thread.start_new_thread( stream, () )
+	app.run()
+	print("Stopped")
