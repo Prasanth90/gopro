42fd436896a255b5872344caf35e8003de0eb729
prasanth <santhtce@gmail.com>
2017-09-23 18:24:14 -0300

    Add mongo db to store urls of screenshots; Thanks to Vaishnavi for fixing camera hung issues;

--- server/gopro.py
+++ server/gopro.py
@@ -1,20 +1,27 @@
 from flask import Flask, abort, request 
+from flask import render_template
 import time
 import _thread
 from goprocam import GoProCamera
 from goprocam import constants
 import base64
 import re
+import requests
+from pymongo import MongoClient
 
 
 app = Flask(__name__)
+client = MongoClient()
+db = client.gopro
+collection = db.order_ids
+
 print("Establishing Connection with the Camera")
 gopro = GoProCamera.GoPro(constants.gpcontrol)
 	
 		
+		
 def stream():
 	print("Starting the live stream")
-	gopro.livestream("start")
 	gopro.stream("udp://127.0.0.1:1000", "high")
 	print("Stopping the live stream")
 
@@ -22,17 +29,23 @@ def stream():
 @app.route('/')
 def hello():
 	return "Hello World"
+	
+@app.route('/images')
+def show_images():
+	items = collection.find({})
+	return render_template('index.html', items=items)
 
 @app.route('/screenshot', methods=['POST'])
-def get_image():
-    image_b64 = request.values['imageBase64']
-    order_id = request.values['orderid']
-    print(image_b64)
-    print(order_id)
-    k = base64.b64encode(image_b64.encode())
-    with open("imageToSave.jpg", "wb") as fh:
-    	fh.write(base64.decodebytes(k))
-    return ''
+def take_screenshot():
+	order_id = request.values['orderid']
+	gpcam = GoProCamera.GoPro(constants.gpcontrol)
+	image_url = gpcam.take_photo(5)
+	save_to_database(order_id, image_url)
+	return ''
+
+def save_to_database(order_id, image_url):
+	post = {"order_id": order_id, "image_url": image_url}
+	collection.insert_one(post).inserted_id
 
 if __name__ == '__main__':
 	print("Starting the Web Server")
